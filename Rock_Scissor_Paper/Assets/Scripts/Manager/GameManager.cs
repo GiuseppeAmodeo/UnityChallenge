af735897f32b82feca3a14ae964abeffa2343e06
Peppe <giuseppe.amodeo96@gmail.com>
2022-01-12 16:30:56 +0100

    Update GameManager.cs

--- Rock_Scissor_Paper/Assets/Scripts/Manager/GameManager.cs
+++ Rock_Scissor_Paper/Assets/Scripts/Manager/GameManager.cs
@@ -1,11 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System;
-
-
 public class GameManager : Singleton<GameManager>
 {
 	private int deckCount = 18;
